db5cd4802bb1cffcfa9f958c25a90d98a7571a7f
Adrian Estopace <estopaceadrian@gmail.com>
2022-05-25 22:26:26 +0800

    test c1

---
+++ Coding Challenge 1.js
@@ -0,0 +1,32 @@
+/*Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK 😀*/
+
+let markMass = 78;
+let markHeight = 1.69;
+
+let johnMass = 85;
+let johnHeight = 1.95;
+
+// BMI
+const BMIMark = markMass / markHeight ** 2;
+
+// BMI
+const BMIJohn = johnMass / johnHeight ** 2;
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(BMIJohn, BMIMark, markHigherBMI);
---
+++ assignment.js
@@ -0,0 +1,34 @@
+let country = "Philippines";
+let continent = "Asia";
+let population = 109.6;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = country;
+let language = "Tagalog";
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+const languageName = language;
+const countryName = country;
+const continentName = continent;
+
+let populationhalf = population / 2;
+console.log(populationhalf);
+
+let x = population + 1;
+console.log(x);
+
+let finlandPopulation = 6;
+console.log(finlandPopulation >= population);
+
+let avPopulation = 33;
+console.log(avPopulation >= population);
+
+let description =
+  "Portugal is in Europe, and its 11 million people speak portuguese";
