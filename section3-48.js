a6a65cdba23177bab79f21a5803ab1e11a3c9929
Adrian Estopace <estopaceadrian@gmail.com>
2022-06-24 21:31:08 +0800

    learning basics of js

---
+++ section3-48.js
@@ -0,0 +1,16 @@
+const jonas = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+  true,
+];
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log([i], jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------------------Starting exercise ${exercise}`);
+}
